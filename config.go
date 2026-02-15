5c011383ebe9ad1eec1f7535dfa416e2282d028e
pmaseberg <paulm@grooverlabs.org>
2026-02-15 14:02:18 -0600

    Improving config.yaml parsing

--- config.go
+++ config.go
@@ -3,46 +3,53 @@ package main
 import (
 	"os"
 
-	"gopkg.in/yaml.v3"
+	"github.com/go-playground/validator/v10"
+	"github.com/goccy/go-yaml"
 )
 
 type Config struct {
 	LogLevel   string            `yaml:"log_level"`
-	Interfaces []InterfaceConfig `yaml:"interfaces"`
-	Rules      []Rule            `yaml:"rules"`
+	Interfaces []InterfaceConfig `yaml:"interfaces" validate:"required,dive"`
+	Rules      []Rule            `yaml:"rules" validate:"required,dive"`
 }
 
 type InterfaceConfig struct {
-	Name  string `yaml:"name"`
-	Group string `yaml:"group"`
+	Name  string `yaml:"name" validate:"required"`
+	Group string `yaml:"group" validate:"required"`
 }
 
 type Rule struct {
-	From   string   `yaml:"from"`
-	To     []string `yaml:"to"`
+	From   string   `yaml:"from" validate:"required"`
+	To     []string `yaml:"to" validate:"required"`
 	Filter Filter   `yaml:"filter"`
-	Types  []string `yaml:"types"` // "query" or "response"
+	Types  []string `yaml:"types"`
 }
 
 type Filter struct {
-	AllowedIPs      []string `yaml:"allowed_ips"`
+	AllowedIPs []string `yaml:"allowed_ips" validate:"dive,ip"`
 	AllowedServices []string `yaml:"allowed_services"`
 }
 
 func LoadConfig(path string) (*Config, error) {
+
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	// Debug: print first 500 chars of config
-	// log.Printf("Raw Config Content:\n%s", string(data))
-
 	var config Config
+
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
 	}
 
+	// Validate the config
+	validate := validator.New()
+	if err := validate.Struct(&config); err != nil {
+		return nil, err
+	}
+
 	return &config, nil
+
 }
